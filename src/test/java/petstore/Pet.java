5cdafc27315f5e886fca7bbb35a197ad4f88c201
MahAmorim <amorimmjg@gmail.com>
2021-10-03 15:03:51 -0300

    :white_check_mark: Created test to include pet

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,50 @@
+// 1 - Pacote
+package petstore;
+
+// 2 - Bibliotecas
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
+
+
+
+// 3 - Classe
+public class Pet {
+    // 3.1 - Atributos
+    String uri = "https://petstore.swagger.io/v2/pet"; //uri ou url: endereço da interface da entidade Pet
+
+    // 3.2 - Métodos e Funções
+
+    @Test(priority = 1)  // Anotação que identifica o teste
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    //Incluir - Create - Post: Registro
+    @Test(priority = 1)  // Anotacao que identifica como teste para o TestNG
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet_1.json");
+
+        //Sintaxe Gherkin: comunicação estruturada = Dado/Given - Quando/When - Então/Then
+
+        given() // Dado
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when() // Quando
+                .post(uri)
+        .then() // Então
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Coffee"))
+                .body("status", is("available"))
+                .body("category.name", is("cat"))
+                .body("tags.name", contains("sta"));
+    }
+}
